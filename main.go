74f07eee86c53cb3b488f4efb2fc4ecdd23f36d0
Miguel Pedrozo <mpedrozoduran@gmail.com>
2020-05-03 14:08:52 -0500

    Added InitAccount and Transfer operations to chaincode

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"log"
+	"strconv"
+)
+
+type Bank struct {
+	contractapi.Contract
+}
+
+func (b *Bank) InitAccount(ctx contractapi.TransactionContextInterface, account string, initialAmountAccount int) error {
+	log.Println("Calling Bank Init")
+	err := ctx.GetStub().PutState(account, []byte(strconv.Itoa(initialAmountAccount)))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *Bank) Transfer(ctx contractapi.TransactionContextInterface, accountFrom string, accountTo string, amount int) error {
+	log.Println("Calling Bank Transfer")
+	amountFromBytes, err := ctx.GetStub().GetState(accountFrom)
+	if err != nil {
+		return fmt.Errorf("Error trying to get amount from accountFrom %s", accountFrom)
+	}
+	if amountFromBytes == nil {
+		return fmt.Errorf("Entity accountFrom %s not found", accountFrom)
+	}
+	amountFrom, _ := strconv.Atoi(string(amountFromBytes))
+
+	amountToBytes, err := ctx.GetStub().GetState(accountTo)
+	if err != nil {
+		return fmt.Errorf("Error trying to get amount from accountTo %s", accountTo)
+	}
+	if amountToBytes == nil {
+		return fmt.Errorf("Entity accountTo %s not found", accountTo)
+	}
+	amountTo, _ := strconv.Atoi(string(amountToBytes))
+
+	amountFrom = amountFrom - amount
+	amountTo = amountTo + amount
+
+	err = ctx.GetStub().PutState(accountFrom, []byte(strconv.Itoa(amountFrom)))
+	if err != nil {
+		return err
+	}
+
+	err = ctx.GetStub().PutState(accountTo, []byte(strconv.Itoa(amountTo)))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	chainCode, err := contractapi.NewChaincode(new(Bank))
+	if err != nil {
+		panic(err.Error())
+	}
+	if err := chainCode.Start(); err != nil {
+		log.Fatalf("Error starting Banc chaincode: %s", err)
+	}
+}
